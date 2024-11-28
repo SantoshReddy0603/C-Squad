3431fd1f7190311d15737917f36772fc65839fa4
SantoshReddy <venkatsantoshreddy@gmail.com>
2024-11-29 00:04:27 +0530

    Update employee.h

--- employee.h
+++ employee.h
@@ -3,14 +3,13 @@
 
 #include "common.h"
 
-// Function Prototypes for Employee Features
 void viewPersonalDetails(char* username);
 void updatePassword(char* username);
 void requestSalaryIncrement(char* username);
 void viewLeaveBalance(char* username);
 void viewAnnouncements();
 void accessHandbook();
-void employeeMenu(char* username);  // Keep this version, only one function with username
-int employeeLogin(char* username, char* password);  // Keep the password argument for future use
+void employeeMenu(char* username);  
+int employeeLogin(char* username, char* password); 
 
 #endif
